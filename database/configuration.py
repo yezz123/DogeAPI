ca56ea37beebc732ff266305ec674cbfeb7d014b
Yasser Tahiri <yasserth19@gmail.com>
2021-07-17 18:16:51 +0100

    🔑 Fix The Alembic files :rocket:

--- database/configuration.py
+++ database/configuration.py
@@ -4,11 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL='sqlite:///database/blog.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = 'sqlite:///blog.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
