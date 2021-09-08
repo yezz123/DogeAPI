276f998fb96a471bc89a607418a652250b4f5252
Sourcery AI <>
2021-09-08 23:06:59 +0000

    'Refactored by Sourcery'

--- core/auth.py
+++ core/auth.py
@@ -24,12 +24,14 @@ def login(
     ).first()
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
+            status_code=status.HTTP_404_NOT_FOUND, detail='Invalid Credentials'
         )
+
     if not Hash.verify(user.password, request.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect password"
+            status_code=status.HTTP_404_NOT_FOUND, detail='Incorrect password'
         )
+
 
     # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": user.email})
