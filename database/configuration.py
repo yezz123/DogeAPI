1bf75c3d28ce2134a9e9dbf5f5fd986fedc55769
Artyom Vancyan <44609997+ArtyomVancyan@users.noreply.github.com>
2023-06-14 19:21:09 +0400

    Fix environment variable defaults (#20)

--- database/configuration.py
+++ database/configuration.py
@@ -5,8 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database/dogeapi.sqlite" or config(
-    "DATABASE_URL"
+SQLALCHEMY_DATABASE_URL = config(
+    "DATABASE_URL", default="sqlite:///./database/dogeapi.sqlite"
 )
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
