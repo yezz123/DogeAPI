4502739f36234490967f474f797faec57279b6ee
Cesar Rodriguez <4625096+cesar-rodriguez@users.noreply.github.com>
2023-08-30 10:01:03 -0400

    adds load balancer iam policy

--- infrastructure/core/eks/main.tf
+++ infrastructure/core/eks/main.tf
@@ -122,3 +122,22 @@ provider "helm" {
     }
   }
 }
+
+// Add the IAM role for load balancer
+module "vpc_cni_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "load_balancer_controller"
+  attach_load_balancer_controller_policy = true
+
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["default:dogeapi", "canary:dogeapi"]
+    }
+  }
+}
+
