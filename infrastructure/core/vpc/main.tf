8c3912662fa623b5e1601c27de84794c154606c3
Cesar Rodriguez <4625096+cesar-rodriguez@users.noreply.github.com>
2023-08-30 09:36:01 -0400

    adds subnet tags

--- infrastructure/core/vpc/main.tf
+++ infrastructure/core/vpc/main.tf
@@ -52,5 +52,13 @@ module "vpc" {
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
 
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
   tags = local.tags
 }
