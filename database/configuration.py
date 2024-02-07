8b2c8868f8c858ba54a5188817cb9d1fb9edc7a4
Cesar Rodriguez <4625096+cesar-rodriguez@users.noreply.github.com>
2024-02-06 23:02:56 -0500

    reverts db update

--- database/configuration.py
+++ database/configuration.py
@@ -5,8 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Defaults to a local SQLite database sqlite:///./database/dogeapi.sqlite
+# To use a different database, set the DATABASE_URL environment variable
+# Example: DATABASE_URL=postgresql+psycopg2://localhost@dogeapi
 SQLALCHEMY_DATABASE_URL = config(
-    "DATABASE_URL", default="postgresql+psycopg2://localhost/dogeapi"
+    "DATABASE_URL", default="sqlite:///./database/dogeapi.sqlite"
 )
 
 if SQLALCHEMY_DATABASE_URL.startswith("postgresql"):
